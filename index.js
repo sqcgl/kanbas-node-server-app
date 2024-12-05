290cb606d6d07c0dcea216736df16e2a87430613
sqcgl <fchen6140@gmail.com>
2024-12-05 16:10:30 -0500

    a6

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ import "dotenv/config";
 import session from "express-session";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import mongoose from "mongoose";
+
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
